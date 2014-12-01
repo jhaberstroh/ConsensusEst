0f7adddfc8b0daa24065e6e5e6e638074132f547
jhaberstroh, MacBook <jhaberstroh@berkeley.edu>
2014-11-30 20:04:42 -0800

    Minor correction fixed bug causing uncertainty to become unreasonably large

--- consensusest/consensus.py
+++ consensusest/consensus.py
@@ -4,7 +4,7 @@ import numpy.linalg as LA
 from copy import deepcopy
 
 def kalmansmoother_twosensor(data, sensors, duration, dyn_model, 
-        dyn_noise_model, dyn_noise_cov, iters, ep = .1):
+        dyn_noise_model, dyn_noise_cov, iters, ep = 1):
     """
     """
     for s in sensors:
@@ -36,17 +36,20 @@ def kalmansmoother_twosensor(data, sensors, duration, dyn_model,
             yi = sensors[i].u[:,t]
             Si = sensors[i].U[:,:]
             for nbr in graph[i]:
-                yi += sensors[nbr].u[:,t]
-                Si += sensors[nbr].U[:,:]
+                # Do NOT use +=, it will modify the objects
+                # within the elements
+                yi = yi + sensors[nbr].u[:,t]
+                Si = Si + sensors[nbr].U[:,:]
             for it in xrange(iters):
                 Mi = LA.inv(LA.inv(P_prev[i]) + Si)
-                x_hat = x_prev[i] + np.dot(Mi, (yi - np.dot(Si, x_prev[i]) ) )
+                x_hat = x_est[i] + np.dot(Mi, (yi - np.dot(Si, x_est[i]) ) )
                 for nbr in graph[i]:
-                    x_hat += np.dot(Mi, (x_prev[nbr] - x_prev[i])) * ep
-                # Update the state of the filyter
+                    x_hat += np.dot(Mi, (x_est[nbr] - x_est[i])) * ep
+                # Update the state of the filter
                 P_est[i] = np.dot(A, np.dot(Mi, A.T)) + np.dot(B, np.dot(Q, B.T))
                 x_est[i] = np.dot(A, x_hat)
             print "Current estimate from {}: {}".format(i, x_est[i].T)
-      
+            #print "Current error {}: \n{}".format(i, P_est[i])
 
-   
+
+
